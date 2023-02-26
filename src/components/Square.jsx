6a022fd0dcdb259708bad1b1079c38d18428ee9f
g-pg <gabrielpgusso@gmail.com>
2023-02-26 12:08:00 -0300

    installed classNames package

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,67 +1,51 @@
-import React, { memo, useMemo, useEffect } from "react";
+import React, { memo } from "react";
+import classNames from "classnames";
+
+const wordleBG = classNames({
+	"bg-green": props.square.correctPosition && props.square.exists,
+	"bg-yellow": !props.square.correctPosition && props.square.exists,
+	"bg-grey": !props.square.correctPosition && !props.square.exists,
+});
 
 function Square(props) {
-	const square = props.square;
+	function handleChange(event) {
+		props.onChange(event, props.lineIndex, props.index);
+	}
 
-	const wordleBG = useMemo(() => {
-		return getWordleBG();
-	}, [square]);
-
-	function getWordleBG() {
-		if (square.letter) {
-			if (!square.correctPosition && !square.exists) {
-				return "bg-grey";
-			} else if (!square.correctPosition && square.exists) {
-				return "bg-yellow";
-			} else if (square.correctPosition && square.exists) {
-				return "bg-green";
-			}
+	function handleKeyUp(event) {
+		if (
+			props.inputRefs.current[props.index + 1] &&
+			event.target.value !== "" &&
+			event.key !== "Backspace"
+		) {
+			props.inputRefs.current[props.index + 1].current.focus();
+		} else if (
+			props.inputRefs.current[props.index - 1] &&
+			event.key === "Backspace" &&
+			event.target.value === ""
+		) {
+			props.inputRefs.current[props.index - 1].current.focus();
 		}
+	}
+
+	function handleClick(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		event.persist();
+		props.square.letter ? props.onClick(event, props.lineIndex, props.index) : "";
 	}
 
 	return (
 		<input
 			type="text"
 			maxLength="1"
-			className={`wordle-square ${wordleBG}`}
+			className={`wordle-square ${props.square.letter ? wordleBG : ""}`}
 			key={`square_${props.index}`}
 			ref={props.refProp}
-			// value={square.letter}
-			onInput={(event) => {
-				event.persist();
-				let letterInput = event.target.value.toUpperCase();
-				if (letterInput.length > 1) {
-					event.target.value = letterInput[letterInput.length - 1];
-				}
-				if (!letterInput.match(/[A-Z]/g)) {
-					event.target.value = "";
-				}
-			}}
-			onChange={(event) => {
-				event.persist();
-				props.onChange(event, props.lineIndex, props.index);
-			}}
-			onKeyDown={(event) => {
-				if (
-					props.inputRefs.current[props.index + 1] &&
-					event.target.value !== "" &&
-					event.key !== "Backspace"
-				) {
-					props.inputRefs.current[props.index + 1].current.focus();
-				} else if (
-					props.inputRefs.current[props.index - 1] &&
-					event.key === "Backspace" &&
-					event.target.value === ""
-				) {
-					props.inputRefs.current[props.index - 1].current.focus();
-				}
-			}}
-			onClick={(event) => {
-				event.preventDefault();
-				event.stopPropagation();
-				event.persist();
-				square.letter ? props.onClick(event, props.lineIndex, props.index) : "";
-			}}
+			value={props.square.letter}
+			onChange={handleChange}
+			onKeyUp={handleKeyUp}
+			onClick={handleClick}
 		/>
 	);
 }
