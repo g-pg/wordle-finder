578b281f3b64b3f51a51dbfb28e57bb9400ca4af
g-pg <gabrielpgusso@gmail.com>
2023-02-26 10:23:06 -0300

    testing separation of square component

--- src/components/WordleTable.jsx
+++ src/components/WordleTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import WordleLine from "./WordleLine";
 import resetIcon from "../assets/img/reset-icon.svg";
 
