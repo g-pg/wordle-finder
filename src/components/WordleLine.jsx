1774e58764eab4a69480fa1d46be3ae4b8690496
g-pg <gabrielpgusso@gmail.com>
2023-02-26 12:10:15 -0300

    classNames to change square style

--- src/components/WordleLine.jsx
+++ src/components/WordleLine.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useState, useRef, memo, useMemo } from "react";
+import React, { useRef, memo } from "react";
 import Square from "./Square";
 
-export default function WordleLine(props) {
+function WordleLine(props) {
 	const inputRefs = useRef([]);
 
-	const squares = useMemo(() => {
-		return props.wordleLines[0].map((line, i) => {
-			const ref = React.createRef();
-			inputRefs.current[i] = ref;
+	const squares = props.wordleLines[0].map((line, i) => {
+		const ref = React.createRef();
+		inputRefs.current[i] = ref;
 
-			return (
-				<Square
-					key={`line${props.lineIndex}square_${i}`}
-					index={i}
-					lineIndex={props.lineIndex}
-					square={props.wordleLines[props.lineIndex][i]}
-					refProp={ref}
-					inputRefs={inputRefs}
-					onChange={props.changeLetter}
-					onClick={props.changeLetterVal}
-				/>
-			);
-		});
-	}, [props.wordleLines]);
+		return (
+			<Square
+				key={`line${props.lineIndex}square_${i}`}
+				index={i}
+				lineIndex={props.lineIndex}
+				square={props.wordleLines[props.lineIndex][i]}
+				refProp={ref}
+				inputRefs={inputRefs}
+				onChange={props.changeLetter}
+				onClick={props.changeLetterVal}
+			/>
+		);
+	});
+
 	return <div className="wordle-line">{squares}</div>;
 }
+
+export default memo(WordleLine);
